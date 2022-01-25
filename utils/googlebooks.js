178996ecdbe35ecbc9f95c0148432e779b048514
Polo <poloaguirre3421@gmail.com>
2022-01-25 14:08:33 -0700

    Inital Commit

--- utils/googlebooks.js
+++ utils/googlebooks.js
@@ -1,15 +1,26 @@
-const axios = require('axios')
+const axios = require("axios");
 
 module.exports = async (phrase) => {
-    const results = await axios.get('https://www.googleapis.com/books/v1/volumes', {
-        params: {
-            format: 'json',
-            q: `${phrase}`
-        }
-    })
+  const results = await axios.get(
+    "https://www.googleapis.com/books/v1/volumes",
+    {
+      params: {
+        format: "json",
+        q: `${phrase}`,
+      },
+    }
+  );
 
-    return JSON.stringify(results.data);
-}
+  //return JSON.stringify(results.data);
+  let objectData = {
+    data: results.data,
+    status: results.status,
+    statusText: results.statusText,
+    headers: results.headers,
+    requestHeader: results.config.headers,
+  };
+  return objectData;
+};
 
 /*  console.log(results.data);
     console.log(results.status);
